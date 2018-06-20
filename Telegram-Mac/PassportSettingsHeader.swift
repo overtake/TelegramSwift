1eedaf38362d9294116510be458f9858b59f9fec
overtake <>
2018-06-20 17:51:57 +0300

    no message

---
+++ Telegram-Mac/PassportSettingsHeader.swift
@@ -0,0 +1,13 @@
+//
+//  PassportSettingsHeader.swift
+//  Telegram
+//
+//  Created by keepcoder on 12/06/2018.
+//  Copyright © 2018 Telegram. All rights reserved.
+//
+
+import Cocoa
+
+class PassportSettingsHeaderItem: TableRowItem {
+
+}
