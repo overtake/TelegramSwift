753bb908b79a1e850dedfb4410aa60a934773538
Mikhail Filimonov <overtakeful@gmail.com>
2019-08-07 19:47:13 +0300

    no message

---
+++ TGUIKit/TGUIKit/DatePicker.swift
@@ -0,0 +1,19 @@
+//
+//  DatePicker.swift
+//  TGUIKit
+//
+//  Created by Mikhail Filimonov on 07/08/2019.
+//  Copyright © 2019 Telegram. All rights reserved.
+//
+
+import Cocoa
+
+class DatePicker: View {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
