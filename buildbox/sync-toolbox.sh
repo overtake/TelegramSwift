0409cd1b7187d6513ba4e0152c5495d15680efa9
overtake <>
2019-11-08 19:00:43 +0400

    update ci

--- buildbox/sync-toolbox.sh
+++ buildbox/sync-toolbox.sh
@@ -7,10 +7,11 @@ source variables.sh
 
 cd ..
 if [ ! -d "./deploy" ]; then
+    chmod -R a+rX ./
+    mkdir -p ./deploy
     git clone "$deploy_repository" deploy
 else
     cd ./deploy
-    chmod -R a+rX ./
     git reset --hard
     git pull origin master
 fi
