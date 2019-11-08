4677d2f551fad927f971f4bd3370cc50e425a0d8
Mikhail Filimonov <overtakeful@gmail.com>
2019-11-08 13:24:38 +0400

    no message

--- buildbox/beta.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-#set -x
-#set -e
-
-export PATH="$PATH:$PWD/../deploy"
-export PATH="$PATH:$HOME/.fastlane/bin"
-sh ./buildbox/build.sh beta
-source deploy-beta.sh ~/build-beta $PWD Telegram.app ~/.credentials/dsa-beta
