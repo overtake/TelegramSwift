f9587b584636143922e30aa49cf5e93414086447
Mikhail Filimonov <overtakeful@gmail.com>
2019-11-12 21:19:15 +0400

    update ci

--- buildbox/internal.sh
+++ buildbox/internal.sh
@@ -7,5 +7,5 @@ export PATH="$PATH:$HOME/.fastlane/bin"
 
 tag="$1"
 
-#sh ./buildbox/build.sh $tag
+sh ./buildbox/build.sh $tag
 sh deploy-$tag.sh ~/build-$tag $PWD Telegram.app ~/.credentials/dsa-$tag
