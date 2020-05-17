5f45f8088ebf8ce65494cf428bf6400c54e3c928
Mikhail Filimonov <overtakeful@gmail.com>
2020-05-17 16:52:21 +0400

    - remove unused virtual machines

--- buildbox/internal.sh
+++ buildbox/internal.sh
@@ -6,7 +6,7 @@ export PATH="$PATH:$PWD/../deploy"
 export PATH="$PATH:$HOME/.fastlane/bin"
 
 tag="$1"
-
+sh ./buildbox/cleanup-telegram-build-vms.sh
 sh ./buildbox/build.sh $tag
 sh deploy-$tag.sh ~/build-$tag $PWD Telegram.app ~/.credentials/dsa-$tag
 rm -rf ~/build-$tag
