45d0012bf205c3836674e55888d03474c886a236
overtake <>
2020-07-15 14:11:35 +0300

    new api

--- core-xprojects/TgVoipWebrtc/TgVoipWebrtc/TgVoipWebrtc.h
+++ core-xprojects/TgVoipWebrtc/TgVoipWebrtc/TgVoipWebrtc.h
@@ -17,4 +17,4 @@ FOUNDATION_EXPORT const unsigned char TgVoipWebrtcVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <TgVoipWebrtc/PublicHeader.h>
 
 
-#import "OngoingCallThreadLocalContextWebrtc.h"
+#import "OngoingCallThreadLocalContext.h"
