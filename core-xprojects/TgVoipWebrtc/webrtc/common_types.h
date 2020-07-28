e698f0c2b8ca95773abd9bcb4477b144df477bcf
Mikhail Filimonov <overtakeful@gmail.com>
2020-07-28 11:16:18 +0300

    no message

--- core-xprojects/TgVoipWebrtc/webrtc/common_types.h
+++ core-xprojects/TgVoipWebrtc/webrtc/common_types.h
@@ -31,6 +31,14 @@ class FrameCountObserver {
                                  uint32_t ssrc) = 0;
 };
 
+// Callback, used to notify an observer when the overhead per packet
+// has changed.
+class OverheadObserver {
+public:
+    virtual ~OverheadObserver() = default;
+    virtual void OnOverheadChanged(size_t overhead_bytes_per_packet) = 0;
+};
+    
 // ==================================================================
 // Video specific types
 // ==================================================================
