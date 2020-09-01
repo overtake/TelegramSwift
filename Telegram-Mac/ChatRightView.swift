3b9a71a2d1f144caf40a24ebb72f0973134bb135
Mikhail Filimonov <overtakeful@gmail.com>
2020-09-01 12:10:20 +0300

    no message

--- Telegram-Mac/ChatRightView.swift
+++ Telegram-Mac/ChatRightView.swift
@@ -175,11 +175,11 @@ class ChatRightView: View {
                 if let postAuthor = item.postAuthor {
                     postAuthor.1.draw(NSMakeRect(icon.backingSize.width + channelViews.0.size.width + 8 + item.stateOverlayAdditionCorner + viewsOffset, item.isBubbled ? (item.isStateOverlayLayout ? 2 : 1) : 0, postAuthor.0.size.width, postAuthor.0.size.height), in: ctx, backingScaleFactor: backingScaleFactor, backgroundColor: backgroundColor)
                 }
-                
+                viewsOffset += channelViews.0.size.width + icon.size.width + 4
             }
             if let replyCount = item.replyCount {
                 let icon = item.presentation.chat.repliesCountIcon(item)
-                ctx.draw(icon, in: NSMakeRect(replyCount.0.size.width + 2 + item.stateOverlayAdditionCorner + viewsOffset, item.isBubbled ? (item.isStateOverlayLayout ? 1 : 0) : 0, icon.backingSize.width, icon.backingSize.height))
+                ctx.draw(icon, in: NSMakeRect(replyCount.0.size.width + 2 + item.stateOverlayAdditionCorner + viewsOffset, item.isBubbled ? (item.isStateOverlayLayout ? 1 : 0) : 2, icon.backingSize.width, icon.backingSize.height))
                 replyCount.1.draw(NSMakeRect(item.stateOverlayAdditionCorner + viewsOffset, item.isBubbled ? (item.isStateOverlayLayout ? 2 : 1) : 0, replyCount.0.size.width, replyCount.0.size.height), in: ctx, backingScaleFactor: backingScaleFactor, backgroundColor: backgroundColor)
             }
             
