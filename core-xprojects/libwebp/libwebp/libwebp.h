46636a756857f5eb75f3d93c5581208b589f76e0
overtake <>
2020-11-20 21:17:35 +0400

    arm

---
+++ core-xprojects/libwebp/libwebp/libwebp.h
@@ -0,0 +1,19 @@
+//
+//  libwebp.h
+//  libwebp
+//
+//  Created by Mikhail Filimonov on 19/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libwebp.
+FOUNDATION_EXPORT double libwebpVersionNumber;
+
+//! Project version string for libwebp.
+FOUNDATION_EXPORT const unsigned char libwebpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libwebp/PublicHeader.h>
+
+
+CGImageRef convertFromWebP(NSData *data);
