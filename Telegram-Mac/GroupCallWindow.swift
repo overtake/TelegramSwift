2c030488b2377ad8366e3d757a75fb93a9439da0
Mikhail Filimonov <overtakeful@gmail.com>
2020-11-22 17:22:11 +0400

    no message

--- Telegram-Mac/GroupCallWindow.swift
+++ Telegram-Mac/GroupCallWindow.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import TGUIKit
+
+
+final class GroupCallWindow : Window {
+    
+}
+
+
+final class GroupCallContext {
+    let window: GroupCallWindow
+    init() {
+        self.window = GroupCallWindow(contentRect: NSMakeRect(0, 0, 400, 580), styleMask: [.fullSizeContentView, .borderless, .resizable, .miniaturizable, .titled], backing: .buffered, defer: true, screen: NSScreen.main)
+        self.window.minSize = NSMakeSize(400, 580)
+        self.window.isOpaque = true
+        self.window.backgroundColor = .black
+    }
+}
