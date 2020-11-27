174fd0fa6db98ff87d70f7819bd1ef6852d222da
Vladimir Belohradsky <lionsart@seznam.cz>
2020-11-27 16:57:38 +0100

    Update status bar icon color for BigSur
    - from 10.14 there are dynamic colors available to reflect system colors.

--- Telegram-Mac/GlobalBadgeNode.swift
+++ Telegram-Mac/GlobalBadgeNode.swift
@@ -228,9 +228,7 @@ func forceUpdateStatusBarIconByDockTile(sharedContext: SharedAccountContext) {
     if let count = Int(NSApplication.shared.dockTile.badgeLabel ?? "0") {
         var color: NSColor = .black
         if #available(OSX 10.14, *) {
-            if systemAppearance.name != .aqua {
-                color = .white
-            }
+            color = .labelColor
         }
         resourcesQueue.async {
             let icon = generateStatusBarIcon(count, color: color)
