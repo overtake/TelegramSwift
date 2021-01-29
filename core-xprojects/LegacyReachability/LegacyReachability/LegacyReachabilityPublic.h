3e584b60d2afca181391c8a325ab47d5ec24387c
Mikhail Filimonov <overtakeful@gmail.com>
2021-01-29 13:01:50 +0400

    update reachability

--- core-xprojects/LegacyReachability/LegacyReachability/LegacyReachabilityPublic.h
+++ core-xprojects/LegacyReachability/LegacyReachability/LegacyReachabilityPublic.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ReachabilityVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Reachability/PublicHeader.h>
 
-#import <Reachability/Reachability.h>
+#import <LegacyReachability/LegacyReachability.h>
