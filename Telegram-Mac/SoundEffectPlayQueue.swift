a546b5da6889dd9d05d3324ecc51af61b142a23d
overtake <overtakeful@gmail.com>
2021-09-20 19:58:14 +0300

    fixes

--- Telegram-Mac/SoundEffectPlayQueue.swift
+++ Telegram-Mac/SoundEffectPlayQueue.swift
@@ -14,11 +14,11 @@ import TelegramCore
 import TGUIKit
 final class SoundEffectPlay {
     private static var queue: [Int64: MediaPlayer] = [:]
-    static func play(postbox: Postbox, name: String?, type: String = "mp3") {
+    static func play(postbox: Postbox, name: String?, type: String = "mp3", volume: Float = 1.0) {
         if let name = name, let filePath = Bundle.main.path(forResource: name, ofType: type) {
             let id = arc4random64()
             let resource = LocalFileReferenceMediaResource(localFilePath: filePath, randomId: id)
-            let player = MediaPlayer(postbox: postbox, reference: MediaResourceReference.standalone(resource: resource), streamable: false, video: false, preferSoftwareDecoding: false, enableSound: true, volume: 1.0, fetchAutomatically: true)
+            let player = MediaPlayer(postbox: postbox, reference: MediaResourceReference.standalone(resource: resource), streamable: false, video: false, preferSoftwareDecoding: false, enableSound: true, volume: volume, fetchAutomatically: true)
          //   player.setVolume(0.6)
             queue[id] = player
             player.play()
