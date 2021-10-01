42a837a4716bbefa59e9c5edd6b673302e24477c
overtake <overtakeful@gmail.com>
2021-10-01 12:06:59 +0300

    no message

---
+++ Telegram-Mac/RequestJoinChatModalController.swift
@@ -0,0 +1,84 @@
+//
+//  RequestJoinGroupModalController.swift
+//  Telegram
+//
+//  Created by Mikhail Filimonov on 01.10.2021.
+//  Copyright © 2021 Telegram. All rights reserved.
+//
+
+import Foundation
+import TGUIKit
+import SwiftSignalKit
+import TelegramCore
+import Postbox
+
+private final class Arguments {
+    let context: AccountContext
+    init(context: AccountContext) {
+        self.context = context
+    }
+}
+
+private struct State : Equatable {
+
+}
+
+private func entries(_ state: State, arguments: Arguments) -> [InputDataEntry] {
+    var entries:[InputDataEntry] = []
+    
+    var sectionId:Int32 = 0
+    var index: Int32 = 0
+    
+    entries.append(.sectionId(sectionId, type: .normal))
+    sectionId += 1
+  
+    // entries
+    
+    entries.append(.sectionId(sectionId, type: .normal))
+    sectionId += 1
+    
+    return entries
+}
+
+func RequestJoinChatModalController(context: AccountContext, peerId: PeerId) -> InputDataModalController {
+
+    let actionsDisposable = DisposableSet()
+
+    let initialState = State()
+    
+    var close:(()->Void)? = nil
+    
+    let statePromise = ValuePromise(initialState, ignoreRepeated: true)
+    let stateValue = Atomic(value: initialState)
+    let updateState: ((State) -> State) -> Void = { f in
+        statePromise.set(stateValue.modify (f))
+    }
+
+    let arguments = Arguments(context: context)
+    
+    let signal = statePromise.get() |> deliverOnPrepareQueue |> map { state in
+        return InputDataSignalValue(entries: entries(state, arguments: arguments))
+    }
+    
+    let controller = InputDataController(dataSignal: signal, title: "Join Channel")
+    
+    controller.onDeinit = {
+        actionsDisposable.dispose()
+    }
+
+    let modalInteractions = ModalInteractions(acceptTitle: "Request to Join", accept: { [weak controller] in
+        _ = controller?.returnKeyAction()
+    }, drawBorder: true, height: 50, singleButton: true)
+    
+    let modalController = InputDataModalController(controller, modalInteractions: modalInteractions)
+    
+    controller.leftModalHeader = ModalHeaderData(image: theme.icons.modalClose, handler: { [weak modalController] in
+        modalController?.close()
+    })
+    
+    close = { [weak modalController] in
+        modalController?.modal?.close()
+    }
+    
+    return modalController
+}
---
+++ Telegram-Mac/RequestJoinChatRowItem.swift
@@ -0,0 +1,9 @@
+//
+//  RequestJoinChatRowItem.swift
+//  Telegram
+//
+//  Created by Mikhail Filimonov on 01.10.2021.
+//  Copyright © 2021 Telegram. All rights reserved.
+//
+
+import Foundation
