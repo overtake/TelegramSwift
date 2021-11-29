dcda1953a5f188bb9fd636679020080edc65c518
overtake <overtakeful@gmail.com>
2021-11-29 11:58:24 +0400

    ci

--- buildbox/build-vm.sh
+++ buildbox/build-vm.sh
@@ -7,5 +7,6 @@ BUILD_CONFIGURATION=$1
 
 cd ~/build
 sh scripts/configure_frameworks.sh
+export FASTLANE_XCODE_LIST_TIMEOUT=120
 fastlane $BUILD_CONFIGURATION
 tar cf  "./output/Telegram.tar" -C "./output" .
