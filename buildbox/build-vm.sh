c37228f7c7a3b1896f3875bd21f539b67ce00d10
overtake <overtakeful@gmail.com>
2021-11-29 19:25:34 +0400

    ci

--- buildbox/build-vm.sh
+++ buildbox/build-vm.sh
@@ -6,7 +6,7 @@ export PATH="$PATH:$HOME/.fastlane/bin"
 BUILD_CONFIGURATION=$1
 
 cd ~/build
+cp "configurations/${BUILD_CONFIGURATION}.xcconfig" "Telegram-Mac/${BUILD_CONFIGURATION}.xcconfig"
 sh scripts/configure_frameworks.sh
-export FASTLANE_XCODE_LIST_TIMEOUT=120
-fastlane $BUILD_CONFIGURATION
+xcodebuild -workspace Telegram-Mac.xcworkspace -scheme Release -configuration Release
 tar cf  "./output/Telegram.tar" -C "./output" .
