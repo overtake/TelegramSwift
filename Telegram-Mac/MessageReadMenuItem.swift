454d8962a068f5767e6d93bcc9cde544201d875a
overtake <overtakeful@gmail.com>
2021-12-28 20:12:39 +0400

    bug fixes

--- Telegram-Mac/MessageReadMenuItem.swift
+++ Telegram-Mac/MessageReadMenuItem.swift
@@ -248,7 +248,7 @@ final class MessageReadMenuRowItem : AppMenuRowItem {
         let viewSize = NSMakeSize(15 * CGFloat(3) - (CGFloat(3) - 1) * 1, 15)
         size.width += viewSize.width + 6
 
-        size.width += 50
+        size.width += 80
         
         return size
     }
