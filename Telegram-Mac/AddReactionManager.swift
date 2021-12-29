080a873ba266c289f7001a23beb60209b72cfb2e
overtake <overtakeful@gmail.com>
2021-12-29 13:56:56 +0400

    bug fixes

--- Telegram-Mac/AddReactionManager.swift
+++ Telegram-Mac/AddReactionManager.swift
@@ -682,7 +682,7 @@ final class AddReactionManager : NSObject, Notifable {
                             let safeRect = base.insetBy(dx: -base.width * 4, dy: -base.height * 4)
                             
                             if NSPointInRect(inside, safeRect), NSPointInRect(NSMakePoint(base.midX, base.midY), view.tableView.frame) {
-                                delayDisposable.set(delaySignal(0.1).start(completed: { [weak self, weak item, weak view] in
+                                delayDisposable.set(delaySignal(0.4).start(completed: { [weak self, weak item, weak view] in
                                     if let item = item, let view = view {
                                         
                                         let available = filter(available, attr: item.firstMessage?.reactionsAttribute)
