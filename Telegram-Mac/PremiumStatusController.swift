0af048f663fa36123c365c75732dcd06e5aef8be
Mikhail Filimonov <overtakeful@gmail.com>
2022-08-08 17:08:15 +0200

    bugfix

---
+++ Telegram-Mac/PremiumStatusController.swift
@@ -0,0 +1,19 @@
+//
+//  PremiumStatusController.swift
+//  Telegram
+//
+//  Created by Mike Renoir on 08.08.2022.
+//  Copyright © 2022 Telegram. All rights reserved.
+//
+
+import Foundation
+import TGUIKit
+import TelegramCore
+import SwiftSignalKit
+import Postbox
+
+final class PremiumStatusController : TelegramViewController {
+    override init(_ context: AccountContext) {
+        super.init(context)
+    }
+}
