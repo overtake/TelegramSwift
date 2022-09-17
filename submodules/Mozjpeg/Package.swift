72e535356dc8239cee98f8e34d84426fd083a075
Mikhail Filimonov <overtakeful@gmail.com>
2022-09-17 23:03:00 +0400

    min macos v10_12

--- submodules/Mozjpeg/Package.swift
+++ submodules/Mozjpeg/Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "Mozjpeg",
     platforms: [
-        .macOS(.v10_11)
+        .macOS(.v10_12)
     ],
     products: [
         .library(
