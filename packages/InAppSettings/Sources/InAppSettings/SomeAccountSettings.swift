276be53df9025ed71add49e2866b3e1aa51d34d5
Mikhail Filimonov <overtakeful@gmail.com>
2022-12-30 13:55:48 +0400

    new storage usage (beta)

---
+++ packages/InAppSettings/Sources/InAppSettings/SomeAccountSettings.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mike Renoir on 30.12.2022.
+//
+
+import Foundation
+import Postbox
+import SwiftSignalKit
+import TelegramCore
+
+
+public struct SomeAccountSettings: Codable, Equatable {
+    public var lastChatReindexTime: Int32?
+    
+    public static var defaultSettings: SomeAccountSettings {
+        return SomeAccountSettings(lastChatReindexTime: nil)
+    }
+    
+    public init(lastChatReindexTime: Int32?) {
+        self.lastChatReindexTime = lastChatReindexTime
+    }
+
+}
+
+public func updateSomeSettingsInteractively(postbox: Postbox, _ f: @escaping (SomeAccountSettings) -> SomeAccountSettings) -> Signal<Void, NoError> {
+    return postbox.transaction { transaction -> Void in
+        transaction.updatePreferencesEntry(key: ApplicationSpecificPreferencesKeys.someSettings, { entry in
+            let settings = entry?.get(SomeAccountSettings.self) ?? SomeAccountSettings.defaultSettings
+            return PreferencesEntry(f(settings))
+            
+        })
+    }
+}
+
+public func someAccountSetings(postbox: Postbox) -> Signal<SomeAccountSettings, NoError> {
+    return postbox.preferencesView(keys: [ApplicationSpecificPreferencesKeys.someSettings]) |> map { preferences in
+        return preferences.values[ApplicationSpecificPreferencesKeys.someSettings]?.get(SomeAccountSettings.self) ?? SomeAccountSettings.defaultSettings
+    }
+}
