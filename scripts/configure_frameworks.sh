19040b33e6ce8ed7648ce2dbc3a83c7d61550fce
Mike Renoir <>
2023-09-08 18:33:42 +0400

    bug fixes

--- scripts/configure_frameworks.sh
+++ scripts/configure_frameworks.sh
@@ -27,24 +27,24 @@ done
 fi
 
 
-#
-#for (( i=0; i<${arraylength}; i++ ));
-#do
-#    FWNAME=${libname[$i]}
-#    LIB=${libs[$i]}
-#    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-#    BASE_PWD="${SCRIPT_DIR}/../submodules/${LIB}"
-#    OUTPUT_DIR=$( mktemp -d )
-#
-#    COMMON_SETUP=" -project ${SCRIPT_DIR}/../core-xprojects/${LIB}/${FWNAME}.xcodeproj -configuration Release BUILD_LIBRARY_FOR_DISTRIBUTION=YES "
-#
-#
-#    DERIVED_DATA_PATH=$( mktemp -d )
-#    xcrun xcodebuild build \
-#        $COMMON_SETUP \
-#        -scheme "${FWNAME}" \
-#        -derivedDataPath "${DERIVED_DATA_PATH}" \
-#        -destination 'generic/platform=macOS'
-#
-#done
-#
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+    FWNAME=${libname[$i]}
+    LIB=${libs[$i]}
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+    BASE_PWD="${SCRIPT_DIR}/../submodules/${LIB}"
+    OUTPUT_DIR=$( mktemp -d )
+
+    COMMON_SETUP=" -project ${SCRIPT_DIR}/../core-xprojects/${LIB}/${FWNAME}.xcodeproj -configuration Release BUILD_LIBRARY_FOR_DISTRIBUTION=YES "
+
+
+    DERIVED_DATA_PATH=$( mktemp -d )
+    xcrun xcodebuild build \
+        $COMMON_SETUP \
+        -scheme "${FWNAME}" \
+        -derivedDataPath "${DERIVED_DATA_PATH}" \
+        -destination 'generic/platform=macOS'
+
+done
+
