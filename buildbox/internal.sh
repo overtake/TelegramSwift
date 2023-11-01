40aa045d1e1e1b97549982263e99844fbcdabd7b
Mike Renoir <>
2023-11-01 09:18:40 +0400

    bump version

--- buildbox/internal.sh
+++ buildbox/internal.sh
@@ -5,8 +5,15 @@ set -e
 export PATH="$PATH:$PWD/../deploy"
 export PATH="$PATH:$HOME/.fastlane/bin"
 
-tag="$1"
-sh ./buildbox/cleanup-telegram-build-vms.sh
-sh ./buildbox/build.sh $tag
-sh deploy-$tag.sh ~/build-$tag $PWD Telegram.app ~/.credentials/dsa-$tag
+
+xcrun notarytool store-credentials "Telegram" --apple-id "appstore@telegram.org" --team-id 6N38VWS5BX --password tfmu-iwxg-xqth-iqvh
+
+#xcrun notarytool store-credentials $notarytool_keychain_profile --apple-id "${app_store_id}" --team-id "${app_team_id}" --password "${app_store_password}"
+#
+#xcrun notarytool submit "$BUILD_PATH/$ZIP_NAME" --keychain-profile $notarytool_keychain_profile --wait
+#
+#tag="$1"
+#sh ./buildbox/cleanup-telegram-build-vms.sh
+#sh ./buildbox/build.sh $tag
+#sh deploy-$tag.sh ~/build-$tag $PWD Telegram.app ~/.credentials/dsa-$tag
 #rm -rf ~/build-$tag
