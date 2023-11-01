a1482d0e330248b7aab38636d87a7396aa9358d9
Mikhail Filimonov <overtakeful@gmail.com>
2023-11-01 09:20:42 +0400

    bugfixes

--- buildbox/internal.sh
+++ buildbox/internal.sh
@@ -8,6 +8,8 @@ export PATH="$PATH:$HOME/.fastlane/bin"
 
 xcrun notarytool store-credentials "Telegram" --apple-id "appstore@telegram.org" --team-id 6N38VWS5BX --password tfmu-iwxg-xqth-iqvh
 
+ xcrun notarytool submit '~/overtake/build-Stable/Telegram-10.2.254873.app.zip' --keychain-profile "Telegram" --wait
+
 #xcrun notarytool store-credentials $notarytool_keychain_profile --apple-id "${app_store_id}" --team-id "${app_team_id}" --password "${app_store_password}"
 #
 #xcrun notarytool submit "$BUILD_PATH/$ZIP_NAME" --keychain-profile $notarytool_keychain_profile --wait
