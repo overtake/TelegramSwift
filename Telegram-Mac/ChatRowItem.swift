0d527300a07f9a97bf15861efb474895443541bb
Mike Renoir <>
2023-11-10 21:36:39 +0400

    bugfixes

--- Telegram-Mac/ChatRowItem.swift
+++ Telegram-Mac/ChatRowItem.swift
@@ -2421,6 +2421,7 @@ class ChatRowItem: TableRowItem {
         commentsBubbleDataOverlay?.makeSize()
         commentsData?.makeSize()
         
+        _commentsBubbleData?.drawBorder =  !isBubbleFullFilled || !captionLayouts.isEmpty
         
        
         
