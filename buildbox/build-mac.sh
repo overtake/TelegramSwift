4e8fac2e63d9a46a3c839292ffd0edb1ca50fc2a
Mikhail Filimonov <overtakeful@gmail.com>
2024-01-17 18:22:26 +0400

    bugfixes

--- buildbox/build-mac.sh
+++ buildbox/build-mac.sh
@@ -5,16 +5,16 @@ set -x
 BUILD_CONFIGURATION=$1
 
 CPPATH="../../../../../build-${BUILD_CONFIGURATION}"
-PROJECT="${$CPPATH}/telegrammacos"
+PROJECT="${CPPATH}/telegrammacos"
 
 rsync -av --progress ../telegrammacos $CPPATH
 
-sh "${$PROJECT}/scripts/configure_frameworks.sh"
+sh "${PROJECT}/scripts/configure_frameworks.sh"
 
-xcodebuild archive -workspace "${$PROJECT}/Telegram-Mac.xcworkspace" \
+xcodebuild archive -workspace "${PROJECT}/Telegram-Mac.xcworkspace" \
 -scheme Release \
 -configuration Release \
 -archivePath $CPPATH \
--xcconfig "${$PROJECT}/configurations/${BUILD_CONFIGURATION}.xcconfig"
+-xcconfig "${PROJECT}/configurations/${BUILD_CONFIGURATION}.xcconfig"
 
          
