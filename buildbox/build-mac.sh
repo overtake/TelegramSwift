6e42be099d938e8640cbbe87a814122eb11081dd
Mike Renoir <>
2024-01-17 20:55:15 +0400

    bugfixes

--- buildbox/build-mac.sh
+++ buildbox/build-mac.sh
@@ -18,11 +18,15 @@ sh "scripts/configure_frameworks.sh"
 
 cp "configurations/${BUILD_CONFIGURATION}.xcconfig" "Telegram-Mac/Release.xcconfig"
 
-xcodebuild archive -workspace "Telegram-Mac.xcworkspace" \
--scheme Release \
--configuration Release \
--archivePath ./build-${BUILD_CONFIGURATION}
+# xcodebuild archive -workspace "Telegram-Mac.xcworkspace" \
+# -scheme Release \
+# -configuration Release \
+# -archivePath ../build-${BUILD_CONFIGURATION}
+
+archive="../build-${BUILD_CONFIGURATION}.archive"
+
+cp "${archive}/Products/Applications/Telegram.app" ./Telegram.app
+cp "${archive}/dSYMs/Telegram.app.dSYM" ./Telegram.app.dSYM
 
 
-cd $PWDPATH
-         
+zip ./Telegram.app ./Telegram.app.zip
