9cff1d1cf1eb983cde2d6921685c45a0029d391c
Andrey Filipenkov <decapitator@ukr.net>
2025-06-06 23:34:08 +0300

    fix suggesting stickers from my sets in the Sticker Search screen

--- Telegram-Mac/StickersViewController.swift
+++ Telegram-Mac/StickersViewController.swift
@@ -1533,7 +1533,7 @@ class NStickersViewController: TelegramGenericViewController<NStickersView>, Tab
                 
                 let emojiRelated: Signal<[FoundStickerItem], NoError> = combineLatest(context.sharedContext.inputSource.searchEmoji(postbox: context.account.postbox, engine: context.engine, sharedContext: context.sharedContext, query: searchText, completeMatch: true, checkPrediction: false), input) |> mapToSignal { emojis, input in
                     
-                    return context.engine.stickers.searchStickers(query: searchText, emoticon: [], inputLanguageCode: input) |> map { $0.0 }
+                    return context.engine.stickers.searchStickers(query: searchText, emoticon: emojis, inputLanguageCode: input) |> map { $0.0 }
 //                    return combineLatest(signals) |> map {
 //                        $0.reduce([], { current, value in
 //                            return current + value.0
